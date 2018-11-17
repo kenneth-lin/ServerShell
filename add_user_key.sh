82da7ccfbf91d48cc38bc2283ade49950a7954c5
kenneth-lin <tutmirleid2017@gmail.com>
2018-11-17 23:29:08 +0800

    add user config

---
+++ add_user_key.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+read -p "Please type the user name:" username
+echo "Now create user $username"
+
+useradd $username
+ssh_dir=/home/${username}/.ssh
+mkdir $ssh_dir
+wget "https://raw.githubusercontent.com/kenneth-lin/ServerShell/master/rsa_key/key1" -O  authorized_keys_temp
+cat authorized_keys_temp > ${ssh_dir}/authorized_keys
+rm -f authorized_keys_temp
+
+chmod 755 $ssh_dir
+chmod 644 ${ssh_dir}/authorized_keys
+
+usermod -G $username wheel
+
+echo "$username is created."
+
+#userdel $username
+#rm -rf /home/${username}
